7cf47db44c2c0d9e16c74479c03a7a29886c57c7
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2015-11-23 18:38:19 +0300

    css fixed

--- 2ndcontacts.rb
+++ 2ndcontacts.rb
@@ -29,17 +29,21 @@ b.text_fields[1].set 'hereweare'
 b.buttons.first.click
 
 # b.goto 'https://www.linkedin.com/vsearch/p?title=CEO&openAdvancedForm=true&titleScope=CP&locationType=I&countryCode=gb&f_N=S&f_CS=2,3&rsid=4120029691447840224428&orig=MDYS'
-b.goto 'https://www.linkedin.com/vsearch/p?title=managing%20director&openAdvancedForm=true&titleScope=CP&locationType=I&countryCode=gb&f_CS=3&rsid=4120029691448025268989&orig=FCTD&f_I=43,96&page_num=61&pt=people&f_N=S&openFacets=N,G,CC,I,CS'
+b.goto 'https://www.linkedin.com/vsearch/p?title=managing%20director&openAdvancedForm=true&titleScope=CP&locationType=I&countryCode=gb&f_CS=2&rsid=4120029691448292688355&orig=FCTD&f_I=43,96&pt=people&f_G=gb%3A0&openFacets=N,G,CC,I,L,CS&f_L=en&f_N=S'
 sleep 5
 
 base = load_base
 page = b.element(css: '.active').text.to_i
-while b.element(css: '.next .page-link').exists?
+active_link_selector = 'li.active'
+next_page_link_selector = 'li.next>a.page-link'
+
+while b.element(css: next_page_link_selector).exists?
   break if page > 100
   url = b.url
   p 'waiting'
-  Watir::Wait.until {   b.element(css: '.active').exist? && b.element(css: '.active').text == page.to_s && b.elements(css: '.main-headline').to_a.size == 10 }
-  p [b.element(css: '.active').text, page]
+  b.element(css: 'li.active').wait_until_present
+  Watir::Wait.until {   b.element(css: active_link_selector).exist? && b.element(css: active_link_selector).text == page.to_s && b.elements(css: '.main-headline').to_a.size == 10 }
+  p [b.element(css: active_link_selector).text, page]
   b.elements(css: '.result').to_a.each_index do |i|
 
     item = b.elements(css: '.result')[i]
@@ -74,6 +78,7 @@ while b.element(css: '.next .page-link').exists?
   
   sleep(rand(0.9..3.2))    
   page = page + 1
-  b.element(css: '.next .page-link').click
+  Watir::Wait.until {   b.element(css: next_page_link_selector).exist? }
+  b.element(css: next_page_link_selector).click
   
 end 
