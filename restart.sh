c29d83d223ea3146fb212ab3a4ffb7b1821a057b
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2016-01-30 03:26:55 +0300

    Owner & Timestamp added

--- restart.sh
+++ restart.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+echo current Thin ID: $(ps aux | grep thin | awk '{print $2}')
 echo killing Thin..
 kill $(ps aux | grep thin | awk '{print $2}')
 
 echo start thin again..
 nohup thin start -a 0.0.0.0 > /dev/null 2>&1 &
+
+echo new Thin ID: $(ps aux | grep thin | awk '{print $2}')
