5a22aca786e81f7aabc20d1247f95519d419555f
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2016-04-28 17:42:43 +0300

    miner updated, logs improved

--- app/jobs/miner.rb
+++ app/jobs/miner.rb
@@ -4,35 +4,38 @@ class Miner < ActiveJob::Base
   queue_as :default
 
   def perform(params)
-    Delayed::Worker.logger.warn('Mining started' + params.to_s)
-
     industry = Industry.find(params[:industry]).name || 'Transportation/Trucking/Railroad'
     n = params[:number].to_i || 100
+    Delayed::Worker.logger.warn('Mining started, ' + industry + ', ' + n)
 
     processed = 0
+    skipped = 0
+    not_found = 0
     emails_before = Person.has_emails.size
 
     people = Person.to_be_mined.where(industry: industry).limit(n)
     people.each do |p|
       if p.name.split(' ').size != 2
         p.update(notes: '{}', email: '')
+        skipped+=1
         next
       end
       first, last = p.name.split(' ')
 
       if last.include?('.') or first.include?('.') or last.size == 1 or first.size == 1
         p.update(notes: '{}', email: '')
+        skipped+=1
         next
       end
 
       person = Pipl::Person.new
       person.add_field Pipl::Name.new(first: first, last: last)
-      # person.add_field Pipl::UserID.new content: "#{linkedin_id}@linkedin"
-      person.add_field Pipl::UserID.new content: "#{p.linkedin_id}@linkedin"
-      response = Pipl::client.search person: person, api_key: 'pije3hnj534fimtabpzx5fgn'
+      person.add_field Pipl::UserID.new content: "#{p.linkedin_id}@linkedin".freeze
+      response = Pipl::client.search person: person, api_key: 'pije3hnj534fimtabpzx5fgn'.freeze
 
       if response.person.nil?
         p.update(notes: '{}', email: '')
+        not_found+=1
         next
       end
       emails = response.person.emails.map(&:address)
@@ -52,6 +55,7 @@ class Miner < ActiveJob::Base
     end
 
     emails_after = Person.has_emails.size
-    Delayed::Worker.logger.warn("Mining finished, #{processed} API requests made, #{emails_after-emails_before} emails found")
+    Delayed::Worker.logger.warn("Mining finished, #{processed} API requests made, #{emails_after-emails_before}" <<
+                                    " emails found, #{not_found} not found, #{skipped} skipped")
   end
 end
